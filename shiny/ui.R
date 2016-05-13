e14c27cbf90812544be7977ad9519457e67affa7
Sridhar Pilli <spilli27@gmail.com>
2016-05-12 23:29:59 -0700

    updated ver2

--- shiny/ui.R
+++ shiny/ui.R
@@ -6,6 +6,10 @@ shinyUI(pageWithSidebar(
     headerPanel("Smart Keyboard - Word predictor"), 
     sidebarPanel(
         textInput(inputId = "textIn", label="Enter text below"),
+        h6('Eg., '),
+        h6('Wish you a Happy birthday'),
+        h6('05 May is cinco de mayo'),
+        h6('This is United States of America'),
         tags$br(),tags$br(),tags$br(),
         p('Author : Sridhar Pilli'),
         p('Email  : spilli27@gmail.com')
@@ -13,7 +17,7 @@ shinyUI(pageWithSidebar(
     mainPanel(
         h4('Summary'),
         h5('This application lets you predict the next word given a sequence of words. 
-           This is done via building language model using n-grams and picking the best possible word'),
+           This is done via language model built using n-grams and picking the highly probable word'),
         
         tags$br(),
         h4('Text entered'),
