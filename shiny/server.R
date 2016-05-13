c6845d78233d515df1b1b5c0aeaa0ef970d60c4d
Sridhar Pilli <spilli27@gmail.com>
2016-05-12 23:23:51 -0700

    working model - ver2

--- shiny/server.R
+++ shiny/server.R
@@ -1,7 +1,8 @@
 # server.r
 
 library(shiny) 
-load("Model.RData")
+load("Model_100K.RData")
+
 ngram_tokenize <- function(data) {
     for(i in 1:length(data)) {
         temp <- data[i]
@@ -20,35 +21,36 @@ ngram_tokenize <- function(data) {
     data <- lapply(data, function(x) { x[!(x %in% remWords)] })
 }
 ngram_predict <- function(data) {
-    #load("Model.RData")
     
     D <- ngram_tokenize(data) # Tokenize
-    D <- unlist(D); D[!(D %in% rownames(u.model))] <- "UNK" # mark the ones not in dictionary as UNK
+    D <- unlist(D); D[!(D %in% names(u.mle))] <- "UNK" # mark the ones not in dictionary as UNK
     L <- length(D)
     
     if(L==0) { P <- "UNK"; Q <- "UNK"}
     if(L==1) { P <- "UNK"; Q <- D[L] }
     if(L>1) { P <- D[L-1]; Q <- D[L] }
-    R <- rownames(u.model); R <- R[-length(R)] # Map against dictionary
+    R <- names(u.mle); R <- R[-length(R)] # Map against dictionary
     
     PQR <- paste(P,Q,R)
-    id1 <- (PQR %in% rownames(t.model))
+    id1 <- (PQR %in% names(t.mle))
     PQ <- paste(P,Q)
     QR <- paste(Q,R)
-    id2 <- (!id1) & (PQ %in% rownames(b.model))    &   (QR %in% rownames(b.model))
-    id3 <- (!id1) & (PQ %in% rownames(b.model))    & (!(QR %in% rownames(b.model)))
-    id4 <- (!id1) & (!(PQ %in% rownames(b.model))) &   (QR %in% rownames(b.model))
-    id5 <- (!id1) & (!(PQ %in% rownames(b.model))) & (!(QR %in% rownames(b.model)))
+    id2 <- (!id1) & (PQ %in% names(b.mle))    &   (QR %in% names(b.mle))
+    id3 <- (!id1) & (PQ %in% names(b.mle))    & (!(QR %in% names(b.mle)))
+    id4 <- (!id1) & (!(PQ %in% names(b.mle))) &   (QR %in% names(b.mle))
+    id5 <- (!id1) & (!(PQ %in% names(b.mle))) & (!(QR %in% names(b.mle)))
     
     a <- matrix(NA,length(PQR),1)
-    if(sum(id1)>0) { a[id1] <- t.model[PQR[id1],2] }
-    if(sum(id2)>0) { a[id2] <- b.model[PQ,2] + b.model[QR[id2],2] + log(2*0.4) }
-    if(sum(id3)>0) { a[id3] <- b.model[PQ,2] + u.model[Q,2] + u.model[R[id3],2] + log(3*0.4) }
-    if(sum(id4)>0) { a[id4] <- u.model[P,2] + u.model[Q,2] + b.model[QR[id4],2] + log(3*0.4) }
-    if(sum(id5)>0) { a[id5] <- u.model[P,2] + u.model[Q,2] + u.model[R[id5],2] + log(4*0.4) }
+    if(sum(id1)>0) { a[id1] <- t.mle[PQR[id1]] }
+    if(sum(id2)>0) { a[id2] <- b.mle[PQ] + b.mle[QR[id2]]            + log(2*0.4) }
+    if(sum(id3)>0) { a[id3] <- b.mle[PQ] + u.mle[Q] + u.mle[R[id3]]  + log(3*0.4) }
+    if(sum(id4)>0) { a[id4] <- u.mle[P]  + u.mle[Q] + b.mle[QR[id4]] + log(3*0.4) }
+    if(sum(id5)>0) { a[id5] <- u.mle[P]  + u.mle[Q] + u.mle[R[id5]]  + log(4*0.4) }
     
-    pred_word <- rownames(u.model)[which.max(a)]
+    pred_word <- R[which.max(a)]
 }
+
+
 shinyServer(
     function(input, output) {
         
